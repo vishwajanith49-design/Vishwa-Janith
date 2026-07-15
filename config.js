a020627b4652f80412fe21353c9805d726736d22
vishwajanith49-design <vishwajanith49@gmail.com>
2026-07-15 22:36:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSON-ID_HERE",
+SESSION_ID: process.env.SESSION_ID || "ZQ9VmIbD#BXe6f7cO5wb7iig8718AUw00HeMyqQk6qWkvnAa-m9c
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
